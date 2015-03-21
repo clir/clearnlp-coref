0fefc12eb730a28a79a5baff77b3f0d737724b5a
Alex Lutz <ajlutz@emory.edu>
2015-03-21 11:09:51 -0400

    added two sieves

---
+++ src/main/java/edu/emory/clir/clearnlp/coreference/ExactStringMatch.java
@@ -0,0 +1,57 @@
+package edu.emory.clir.clearnlp.coreference;
+
+import java.util.List;
+
+import edu.emory.clir.clearnlp.collection.pair.Pair;
+import edu.emory.clir.clearnlp.collection.set.DisjointSet;
+import edu.emory.clir.clearnlp.coreference.mention.AbstractMentionDetector;
+import edu.emory.clir.clearnlp.coreference.mention.EnglishMentionDetector;
+import edu.emory.clir.clearnlp.coreference.mention.Mention;
+import edu.emory.clir.clearnlp.dependency.DEPTree;
+/**
+ * 
+ * @author alexlutz
+ * this will be the first sieve that performs exact string matching between mentions
+ */
+public class ExactStringMatch extends AbstractCoreferenceResolution {
+	
+	private AbstractMentionDetector detector;
+	
+	public ExactStringMatch() 
+	{	
+		detector = new EnglishMentionDetector();	//need to create my own in order to get larger number of mentions
+	}
+	
+	public Pair<List<Mention>,DisjointSet> getEntities(List<DEPTree> trees)
+	{
+		List<Mention> mentions = detector.getMentionList(trees);
+		DisjointSet set = new DisjointSet(mentions.size());
+		int i, j, size = mentions.size();
+		Mention curr, prev;
+		
+		for (i=1; i<size; i++)
+		{
+			curr = mentions.get(i);
+			
+			for (j=i-1; j>=0; j--)
+			{
+				prev = mentions.get(i);
+				
+				if (exactMatch(prev, curr))
+				{
+					set.union(i, j);
+					break;
+				}
+			}
+		}
+		
+		return new Pair<List<Mention>, DisjointSet>(mentions, set);
+	}
+	
+	private boolean exactMatch(Mention prev, Mention curr)
+	{
+		return prev.getNode().getWordForm().equals(curr.getNode().getWordForm());
+	}
+	
+
+}
