c6d76646e2784b09e268df98f1d53a1c04fb0a42
Henry Chen <yuhsinchen19940823@gmail.com>
2015-05-12 13:05:28 -0400

    Repo structural reorganization

--- src/test/java/edu/emory/clir/clearnlp/coreference/mention/detector/EnglishCommonNounDetector.java
+++ src/test/java/edu/emory/clir/clearnlp/coreference/mention/detector/EnglishCommonNounDetector.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.emory.clir.clearnlp.coreference;
+package edu.emory.clir.clearnlp.coreference.mention.detector;
 
 import org.junit.Test;
 
