53afbe63a97b612147ea931d30ee6a94599e6aed
Alex Lutz <ajlutz@emory.edu>
2015-06-19 13:19:47 -0400

    editations

--- src/main/java/edu/emory/clir/clearnlp/coreference/sieve/RuleBasedPleonasticIt.java
+++ src/main/java/edu/emory/clir/clearnlp/coreference/sieve/RuleBasedPleonasticIt.java
@@ -1,6 +1,5 @@
 package edu.emory.clir.clearnlp.coreference.sieve;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -8,7 +7,6 @@ import java.util.function.Predicate;
 import edu.emory.clir.clearnlp.coreference.dictionary.PathSieve;
 import edu.emory.clir.clearnlp.coreference.mention.AbstractMention;
 import edu.emory.clir.clearnlp.coreference.type.AttributeType;
-import edu.emory.clir.clearnlp.dependency.DEPLib;
 import edu.emory.clir.clearnlp.dependency.DEPNode;
 import edu.emory.clir.clearnlp.dependency.DEPTagEn;
 import edu.emory.clir.clearnlp.dependency.DEPTree;
@@ -16,15 +14,15 @@ import edu.emory.clir.clearnlp.pos.POSLibEn;
 import edu.emory.clir.clearnlp.pos.POSTagEn;
 import edu.emory.clir.clearnlp.util.DSUtils;
 import edu.emory.clir.clearnlp.util.IOUtils;
+import edu.emory.clir.clearnlp.util.StringUtils;
 
 /**
  * @author alexlutz
  * @version 1.0
  * @since	6/10/15
- * need to re-look at cognitive noun patterns
- * NEED TO FIX WITH HEAD WORDS SINCE it is really raining DOES NOT HAVE IS AS THE ROOT
- * WANT TO MOVE COG NOUN INTO ITBE PATTTERN
- * MAKE TREE GLOBAL VARIABLE
+ * want to provide more support with questions
+ * want to possibly ignore prepositional phrases in addition to adverbs
+ * maybe add idioms
  */
 public class RuleBasedPleonasticIt
 {
@@ -67,27 +65,27 @@ public class RuleBasedPleonasticIt
 	
 	private boolean isPleonastic(AbstractMention mention)
 	{
-		DEPTree tree = mention.getTree();
+		tree = mention.getTree();
 		int id = mention.getNode().getID();
-		return findItBe(tree, id) || specialConstruct(tree, id) || heuristicSeem(tree, id) || cognitiveNounPattern(tree, id) || nonBePattern(tree, id); 
+		return findItBe(id) || specialConstruct(id) || heuristicSeem(id) || cognitiveNounPattern(id) || nonBePattern(id) || auxIs(id); 
 	}
 
-	private boolean itBeAdj(DEPTree tree, int i)
+	private boolean itBeAdj(int i)
 	{
-		return hasWord(tree, i, "that") || hasPOS(tree, i, isPronoun) || forTo(tree, i) || hasPOSByTag(tree, i, POSTagEn.POS_TO);
+		return hasWord(i, "that") || hasPOS(i, isPronoun) || forTo(i) || hasPOSByTag(i, POSTagEn.POS_TO);
 	}
 	
-	private boolean specialConstruct(DEPTree tree, int id)
+	private boolean specialConstruct(int id)
 	{
-		int i = findPOS(tree, 1, isNoun);
-		i = findPOS(tree, ++i, isVerb);
-		i = findWord(tree, ++i, "it");
-		i = findPOS(tree, ++i, isAdjective);
-//		return forTo(tree, ++i) || hasPOSByTag(tree, ++i, POSTagEn.POS_TO);	//this does not match with the patterns that he gives but matches with his examples
+		int i = findPOS(1, isNoun);
+		i = findPOS(++i, isVerb);
+		i = findWord(++i, "it");
+		i = findPOS(++i, isAdjective);
+//		return forTo(i+1) || hasPOSByTag(i+1, POSTagEn.POS_TO);	//this does not match with the patterns that he gives but matches with his examples
 		return (i < tree.size()) ? true : false;
 	}
 	
-	private boolean findItBe(DEPTree tree, int id)	
+	private boolean findItBe(int id)	
 	{
 		int i = 3, size = tree.size();
 		DEPNode node = tree.get(id);
@@ -95,24 +93,24 @@ public class RuleBasedPleonasticIt
 			for (; i < size; i++) {
 				node = tree.get(i);
 				if 		(isAdverb.test(node.getPOSTag())) continue;
-				else if (hasContainsWord(tree, i, weatherTerms)) return true;
-				else if (hasContainsWord(tree, i, timeWords)) return true;
-				else if (hasContainsWord(tree, i, seasons)) return true;	
+				else if (weatherTerms.contains(node.getLemma())) return true;
+				else if (timeWords.contains(node.getLemma())) return true;
+				else if (seasons.contains(node.getLemma())) return true;	
 				else if (node.isLabel(DEPTagEn.DEP_NUMMOD)) return true;
-				else if (isAdjective.test(node.getPOSTag())) return itBeAdj(tree, ++i);
+				else if (isAdjective.test(node.getPOSTag())) return itBeAdj(++i);
 				return false;
 				}
 			}
 		return false;
 	}
 	
-	private boolean forTo(DEPTree tree, int i)	//need to make the for optional
+	private boolean forTo(int i)	//need to make the for optional
 	{
-		i = findWordWithPOSTag(tree, i, "for", POSTagEn.POS_IN);
-		return (i < tree.size()) ?  hasPOSByTag(tree, i, POSTagEn.POS_TO) : false;
+		i = findWordWithPOSTag(i, "for", POSTagEn.POS_IN);
+		return (i < tree.size()) ?  hasPOSByTag(i, POSTagEn.POS_TO) : false;
 	}
 	
-	private boolean hasPOS(DEPTree tree, int i, Predicate<String> f1)
+	private boolean hasPOS(int i, Predicate<String> f1)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
@@ -130,7 +128,7 @@ public class RuleBasedPleonasticIt
 //		return (a < tree.size()) ?  true : false;
 //	}
 	
-	private int findPOS(DEPTree tree, int i, Predicate<String> f1)
+	private int findPOS(int i, Predicate<String> f1)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
@@ -142,18 +140,29 @@ public class RuleBasedPleonasticIt
 		return size;
 	}
 	
-	private boolean hasPOSByTag(DEPTree tree, int i, String pos)
+	private boolean hasPOSByTag(int i, String pos)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
-			if		(isAdverb.test(pos))		continue;
+			if		(isAdverb.test(tree.get(i).getPOSTag())) continue;
 			else if (tree.get(i).isPOSTag(pos)) return true;
 			return false;
 		}
 		return false;
 	}
 	
-	private boolean hasWord(DEPTree tree, int i, String word)
+	private int findPOSByTag(int i, String pos)
+	{
+		int size = tree.size();
+		for (; i < size; i++) {
+			if		(isAdverb.test(tree.get(i).getPOSTag())) continue;
+			else if (tree.get(i).isPOSTag(pos)) return i;
+			return size;
+		}
+		return size;
+	}
+	
+	private boolean hasWord(int i, String word)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
@@ -171,7 +180,7 @@ public class RuleBasedPleonasticIt
 //		return (a < size) ? true : false;
 //	}	
 	
-	private int findWord(DEPTree tree, int i, String word)
+	private int findWord(int i, String word)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
@@ -183,7 +192,7 @@ public class RuleBasedPleonasticIt
 		return size;
 	}
 	
-//	private boolean hasWordWithPOSTag(DEPTree tree, int i, String word, String pos)
+//	private boolean hasWordWithPOSTag(int i, String word, String pos)
 //	{
 //		int size = tree.size();
 //		for (; i < size; i++) {
@@ -194,7 +203,7 @@ public class RuleBasedPleonasticIt
 //		return false;
 //	}
 	
-	private int findWordWithPOSTag(DEPTree tree, int i, String word, String pos)
+	private int findWordWithPOSTag(int i, String word, String pos)
 	{
 		int size = tree.size();
 		for (; i < size; i++) {
@@ -206,63 +215,63 @@ public class RuleBasedPleonasticIt
 		return size;
 	}
 	
-	private boolean hasContainsWord(DEPTree tree, int i, Set<String> container)
+	private int findContainsWord(int i, Set<String> container)
 	{
 		int size = tree.size();
-		for (; i < size; i++) {
+		for (; i < size; i++){
 			DEPNode node = tree.get(i);
 			if (isAdverb.test(node.getPOSTag())) continue;
-			else if (container.contains(node.getWordForm())) return true;
-			return false;
+			else if (container.contains(node.getWordForm())) return i;
+			return size;
 		}
-		return false;
+		return size;
 	}
 	
-//	private boolean hasContainsWord(DEPTree tree, int i, Set<String> container)
-//	{
-//		int a = findContainsWord(tree, i, container);
-//		return (a < size) ? true : false;
-//	}
-//	
-//	private int findContainsWord(DEPTree tree, int i, Set<String> container)
-//	{
-//		int size = tree.size();
-//		for (; i < size; i++){
-//			DEPNode node = tree.get(i);
-//			if (isAdverb.test(node.getPOSTag())) continue;
-//			else if (container.contains(node.getWordForm())) return i;
-//			return size;
-//		}
-//		return size;
-//	}
-	
-	private boolean heuristicSeem(DEPTree tree, int id)	//missing one
+	private boolean heuristicSeem(int id)	
 	{
-		return tree.get(id).getHead().getLemma().equals("seem") && !hasPOSByTag(tree, tree.getFirstRoot().getID(), POSTagEn.POS_TO);
+		return tree.get(id).getHead().getLemma().equals("seem") && !hasPOSByTag(tree.getFirstRoot().getID(), POSTagEn.POS_TO);
 	}
 	
-	private boolean cognitiveNounPattern(DEPTree tree, int id)	
+	private boolean cognitiveNounPattern(int id)	
 	{
 		DEPNode node = tree.get(id);
-		return node.getHead().getLemma().equals("be") && cogSplit(tree);
+		return node.getHead().getWordForm().equals("is") && cogSplit(node.getHead().getID());
 	}
 	
-	private boolean cogSplit(DEPTree tree)	//need better
+	private boolean cogSplit(int i)	
 	{
-		if (tree.get(3).isLabel(DEPTagEn.DEP_DET)) return cogEnd(tree);
-		else if (tree.get(3).getLemma().equals("on") && tree.get(4).isLabel(DEPTagEn.DEP_DET)) return cogEnd(tree);
+		int answer = findWord(i+1, "on");
+		answer = findPOSByTag(++answer, POSTagEn.POS_DT);
+		if (answer < tree.size()) return cogEnd(answer);
+		else {
+			answer = findPOSByTag(i+1, POSTagEn.POS_DT);
+			return cogEnd(answer);
+		}
+	}
+	
+	private boolean cogEnd(int i)
+	{
+		i = findContainsWord(++i, cognitiveNouns);
+		return hasWord(++i, "that");
+	}
+	
+	private boolean nonBePattern(int id)
+	{
+		DEPNode node = tree.get(id).getHead();	
+		if(node.isPOSTag("VBD") || node.isPOSTag("VBN") && cognitiveVerbs.contains(node.getLemma())) return hasWord(node.getID()+1, "that");
 		return false;
 	}
 	
-	private boolean cogEnd(DEPTree tree)
+	private boolean auxIs(int id)
 	{
-		return cognitiveNouns.contains(tree.get(4).getWordForm()) && tree.get(5).getLemma().equals("lemma");
-	}
-	
-	private boolean nonBePattern(DEPTree tree, int id)
-	{
-		DEPNode node = tree.getFirstRoot();	
-		if(node.isPOSTag("VBD") || node.isPOSTag("VBN") && cognitiveVerbs.contains(node.getLemma())) return hasWord(tree, node.getID()+1, "that");
+		int size = tree.size();
+		int i = findWord(id+1, "be");
+		for (; i < size; i++) {
+			DEPNode node = tree.get(++i);
+			if (isAdverb.test(node.getPOSTag())) continue;
+			else if (weatherTerms.contains(StringUtils.toLowerCase(node.getWordForm()))) return true;
+			return false;
+		}
 		return false;
 	}
 }
--- src/test/java/edu/emory/clir/clearnlp/coreference/coref/sieve/RuleBasedPleonasticItTest.java
+++ src/test/java/edu/emory/clir/clearnlp/coreference/coref/sieve/RuleBasedPleonasticItTest.java
@@ -1,7 +1,5 @@
 package edu.emory.clir.clearnlp.coreference.coref.sieve;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -13,12 +11,13 @@ import edu.emory.clir.clearnlp.coreference.SieveSystemCoreferenceResolution;
 import edu.emory.clir.clearnlp.coreference.config.SieveSystemCongiuration;
 import edu.emory.clir.clearnlp.coreference.mention.AbstractMention;
 import edu.emory.clir.clearnlp.coreference.path.PathData;
-import edu.emory.clir.clearnlp.coreference.sieve.IndefinitePronounMatch;
 import edu.emory.clir.clearnlp.coreference.sieve.RuleBasedPleonasticIt;
 import edu.emory.clir.clearnlp.coreference.utils.CoreferenceTestUtil;
 import edu.emory.clir.clearnlp.coreference.utils.structures.CoreferantSet;
+import edu.emory.clir.clearnlp.coreference.utils.util.CorpusConverter;
 import edu.emory.clir.clearnlp.dependency.DEPTree;
 import edu.emory.clir.clearnlp.util.FileUtils;
+import edu.emory.clir.clearnlp.util.IOUtils;
 import edu.emory.clir.clearnlp.util.lang.TLanguage;
 
 /**
@@ -36,40 +35,40 @@ public class RuleBasedPleonasticItTest
 	@Test
 	public void test()
 	{
-		 SieveSystemCongiuration config = new SieveSystemCongiuration(TLanguage.ENGLISH);
-	        config.loadDefaultMentionDectors();
-	        config.loadDefaultSieves(false);
-	        AbstractCoreferenceResolution coref = new SieveSystemCoreferenceResolution(config);
+		SieveSystemCongiuration config = new SieveSystemCongiuration(TLanguage.ENGLISH);
+		config.loadDefaultMentionDectors();
+		config.loadDefaultSieves(false);
+		AbstractCoreferenceResolution coref = new SieveSystemCoreferenceResolution(config);
 
-	        List<String> l_filePaths = FileUtils.getFileList(PathData.ENG_COREF_MICROSOFT_PARSED_DIR, ".cnlp", false);
-	        List<DEPTree> trees;
-//	        for(String filePath : l_filePaths) {
-//	            trees = CoreferenceTestUtil.getTestDocuments(filePath, 9);
-//	            resolution = coref.getEntities(trees);
-//	            long start = System.currentTimeMillis();
-//	            test.removePleonasticIt(resolution.o1);
-//	            long end = System.currentTimeMillis();
-//	            
-//	            System.out.println(end - start);
-//		}
-//	        testSentence(coref, "It is rainy.");
-//	        testSentence(coref, "It is autumn.");
-//	        testSentence(coref, "It is 3 pm.");
-//	        testSentence(coref, "It is time to go home");
-//	        testSentence(coref, "It is quite late");
-//	        testSentence(coref, "It is very likely she will come");
-//	        testSentence(coref, "It seems that he has arrived.");
-//	        testSentence(coref, "It seems unlikely that he will finish.");
-//	        testSentence(coref, "It is necessary that you check the levels before starting");
-//	        testSentence(coref, "It was thought that we would finish on time today.");
-	        testSentence(coref, "It is the plan that we will wake up at dawn.");	//fails
-	        testSentence(coref, "It is on this conclusion that I base my work.");	//fails
-//	        testSentence(coref, "It seemed it was the case.");
+		List<String> l_filePaths = FileUtils.getFileList(PathData.ENG_COREF_MICROSOFT_PARSED_DIR, ".cnlp", false);
+		for(String filePath : l_filePaths) { 	
+			trees = CoreferenceTestUtil.getTestDocuments(filePath, 9);
+			resolution = coref.getEntities(trees);
+			test.removePleonasticIt(resolution.o1);
+
+		}
+	        testSentence(coref, "It seems that he has arrived.");
+	        testSentence(coref, "It seems unlikely that he will finish.");
+	        testSentence(coref, "It seemed it was the case.");
+	        testSentence(coref, "It is 3 pm.");
+	        testSentence(coref, "It is time to go home");
+	        testSentence(coref, "It is quite late");
+	        testSentence(coref, "It is rainy.");
+	        testSentence(coref, "It is raining");
+	        testSentence(coref, "It is autumn.");
+	        testSentence(coref, "It is funny that he left so early.");
+	        testSentence(coref, "It is odd to walk that way.");	
 	        testSentence(coref, "We made it certain that we would succeed."); //latest version works but is not what the pattern is
-//	        testSentence(coref, "It was obvious I could do it.");
-//	        testSentence(coref, "It is odd to walk that way.");	
-//	        testSentence(coref, "It is funny that he left so early.");
-//	        testSentence(coref, "It was agreed that we would eat at noon.");	//need to double check cases like this
+	        testSentence(coref, "It was thought that we would finish on time today.");
+	        testSentence(coref, "It was obvious I could do it.");
+	        testSentence(coref, "It is the plan that we will wake up at dawn.");	
+	        testSentence(coref, "It is on this conclusion that I base my work.");	
+	        testSentence(coref, "It is very likely she will come");
+	        testSentence(coref, "It is necessary that you check the levels before starting");
+	        testSentence(coref, "It was agreed that we would eat at noon.");	//need to double check cases like this
+	        testSentence(coref, "It was agreed that the transaction would be kept secret.");
+	        testSentence(coref, "John found it difficult to speak");
+	        
 	}
 	
 	private void testSentence(AbstractCoreferenceResolution coref, String line)
