7dfe6b7515c82818e54a7749d52199d8360475a6
Henry Chen <yuhsinchen19940823@gmail.com>
2015-06-23 16:37:48 -0400

    Created wordnet library

---
+++ src/main/java/edu/emory/clir/clearnlp/coreference/dictionary/wordnet/WordNetPointerSymbols.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015, Emory University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.clir.clearnlp.coreference.dictionary.wordnet;
+
+/**
+ * @author 	Yu-Hsin(Henry) Chen ({@code yu-hsin.chen@emory.edu})
+ * @version	1.0
+ * @since 	Jun 23, 2015
+ */
+public interface WordNetPointerSymbols {
+	String 	Antonym = "!",
+			Hypernym = "@",
+			InstanceHypernym = "@i",
+			Hyponym = "~",
+			InstanceHyponym = "~i",
+			MemberHolonym = "#m",
+			SubstanceHolonym = "#s",
+			PartHolonym = "#p",
+			MemberMeronym = "%m",
+			SubstanceMeronym = "%s",
+			PartMeronym = "%p",
+			Attribute = "=",
+			DerivationallyRelatedForm = "+",
+			DomainOfTopicSynset = ";c",
+			MemberOfTopicDomain = "-c",
+			DomainOfRegionSynset = ";r",
+			MemberOfRegionDomain = "-r",
+			DomainOfUsageSynset = ";u",
+			MemberOfUsageDomain = "-u"; 
+}
