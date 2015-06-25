794ab3f1be194f8c5a30510ffbd0287eeb7d3fc3
Henry Chen <yuhsinchen19940823@gmail.com>
2015-06-25 16:55:08 -0400

    Update components

--- src/main/java/edu/emory/clir/clearnlp/coreference/components/CoreferenceFeatureExtractor.java
+++ src/main/java/edu/emory/clir/clearnlp/coreference/components/CoreferenceFeatureExtractor.java
@@ -18,14 +18,15 @@ package edu.emory.clir.clearnlp.coreference.components;
 import java.util.List;
 
 import edu.emory.clir.clearnlp.classification.vector.StringFeatureVector;
+import edu.emory.clir.clearnlp.coreference.dictionary.PathDictionary;
 import edu.emory.clir.clearnlp.coreference.mention.AbstractMention;
-import edu.emory.clir.clearnlp.coreference.sieve.ExactStringMatch;
-import edu.emory.clir.clearnlp.coreference.sieve.PreciseConstructMatch;
-import edu.emory.clir.clearnlp.coreference.sieve.RelaxedStringMatch;
 import edu.emory.clir.clearnlp.coreference.type.AttributeType;
 import edu.emory.clir.clearnlp.coreference.type.FeatureType;
 import edu.emory.clir.clearnlp.coreference.type.PronounType;
+import edu.emory.clir.clearnlp.coreference.utils.retriever.wordnet.WordNetNounEntryRetriever;
 import edu.emory.clir.clearnlp.coreference.utils.structures.CoreferantSet;
+import edu.emory.clir.clearnlp.coreference.utils.util.CoreferenceDSUtils;
+import edu.emory.clir.clearnlp.coreference.utils.util.CoreferenceStringUtils;
 import edu.emory.clir.clearnlp.dependency.DEPNode;
 import edu.emory.clir.clearnlp.dependency.DEPTree;
 import edu.emory.clir.clearnlp.util.constant.CharConst;
@@ -35,105 +36,137 @@ import edu.emory.clir.clearnlp.util.constant.CharConst;
  * @version	1.0
  * @since 	Jun 9, 2015
  */
-public class CoreferenceFeatureExtractor implements FeatureType{
-	private ExactStringMatch exactString = new ExactStringMatch(true);
-	private RelaxedStringMatch relaxedString = new RelaxedStringMatch(true);
-	private PreciseConstructMatch preiseConstruct = new PreciseConstructMatch();
-	
+public class CoreferenceFeatureExtractor implements FeatureType {	
 	private StringBuilder s_builder = new StringBuilder();
+	private WordNetNounEntryRetriever wordnetDb = new WordNetNounEntryRetriever(PathDictionary.ENG_WORDNETNOUNS);
 
 	public StringFeatureVector getFeatures(List<DEPTree> trees, CoreferantSet bootstrapLinks, AbstractMention mention1, DEPTree tree1, AbstractMention mention2, DEPTree tree2){
 		StringFeatureVector vector = new StringFeatureVector();
+		
+		DEPNode node1 = mention1.getNode(), node2 = mention2.getNode();
 		int multiMentionIndex = getMultiMentionIndex(mention1, mention2);
+		String wordForm1 = mention1.getWordFrom(), wordForm2 = mention2.getWordFrom();
+		PronounType pronoun1 = mention1.getPronounType(), pronoun2 = mention2.getPronounType();
 		
 		/* Global features */
 		/* Match Gender */
-		vector.addFeature(GenderMatch, getStringLabel(mention1.getGenderType().toString(), mention2.getGenderType().toString()));
-			
+		vector.addFeature(Attributes1, mention1.getGenderType().toString());
+		vector.addFeature(Attributes2, mention2.getGenderType().toString());
+		
 		/* Match Number */
-		vector.addFeature(NumberMatch, getStringLabel(mention1.getNumberType().toString(), mention2.getNumberType().toString()));
+		vector.addFeature(Attributes1, mention1.getNumberType().toString());
+		vector.addFeature(Attributes2, mention2.getNumberType().toString());
 		
 		/* Match Entity */
-		vector.addFeature(EntityMatch, getStringLabel(mention1.getEntityType().toString(), mention2.getEntityType().toString()));
+		vector.addFeature(Attributes1, mention1.getEntityType().toString());
+		vector.addFeature(Attributes2, mention2.getEntityType().toString());
+		
+		/* Match Named Entity */
+		if(node1 != null)	vector.addFeature(Attributes1, node1.getNamedEntityTag());
+		if(node2 != null)	vector.addFeature(Attributes2, node2.getNamedEntityTag());
 		
 		/* Match Pronoun */
-		PronounType pronoun1 = mention1.getPronounType(), pronoun2 = mention2.getPronounType();
-		String s_pronoun1 = (pronoun1 == null)? NULL : pronoun1.toString(), s_pronoun2 = (pronoun2 == null)? NULL : pronoun2.toString();
-		vector.addFeature(PronounMatch, getStringLabel(s_pronoun1, s_pronoun2));
+		if(pronoun1 != null)	vector.addFeature(Attributes1, mention1.getPronounType().toString());
+		if(pronoun2 != null)	vector.addFeature(Attributes2, mention2.getPronounType().toString());
 		
-		/* Is named entity-pronoun pair */
-		vector.addFeature(NEPronounPair, getBinaryLabel((mention1.isNameEntity() && pronoun2 != null) || (pronoun1 != null && mention2.isNameEntity())));
-		
-		/* Multiple mention index */
-		vector.addFeature(MultiMentionIndex, Integer.toString(multiMentionIndex));
+		/* Speaker status */
+		if(mention1.hasAttribute(AttributeType.QUOTE))	vector.addFeature(Attributes1, HasQuote1);
+		if(mention2.hasAttribute(AttributeType.QUOTE))	vector.addFeature(Attributes2, HasQuote2);
 		
 		/* Particular mention type features */
 		switch(multiMentionIndex){
 			case -1:	// Both are single metnions
-				DEPNode node1 = mention1.getNode(), node2 = mention2.getNode();
-				
-				/* --- Sieve rules features --- */
-				/* Speaker status */
-				vector.addFeature(SpeakerStatus, getStringLabel(getBinaryLabel(mention1.hasAttribute(AttributeType.QUOTE)), getBinaryLabel(mention2.hasAttribute(AttributeType.QUOTE))));
-				
 				/* Exact String Match */
-				vector.addFeature(ExactString, getBinaryLabel(exactString.match(mention1, mention2)));
+				if(mention1.getSubTreeWordSequence(true).equals(mention2.getSubTreeWordSequence(true)))
+					vector.addFeature(Boolean, Integer.toString(ExactStringMatch));
 				
-				/* Relaxed String Match */
-				vector.addFeature(RelaxedString, getBinaryLabel(relaxedString.match(mention1, mention2)));
-				
-				/* Match precise construct */
-				vector.addFeature(AppositionMatch, getBinaryLabel(preiseConstruct.appositiveMatch(mention1, mention2)));
-				vector.addFeature(PredicateNomMatch, getBinaryLabel(preiseConstruct.predicateNominativeMatch(mention1, mention2)));
-				vector.addFeature(AcronymMatch, getBinaryLabel(preiseConstruct.acronymMatch(mention1, mention2)));
-				
-				/* --- Tree information features --- */
 				/* POS tags */
 				vector.addFeature(CurrentPOSTag, getStringLabel(node1.getPOSTag(), node2.getPOSTag()));
-				
-				/* Head node POS tags */
-				vector.addFeature(HeadNodePOSTag, getStringLabel(node1.getHead().getPOSTag(), node2.getHead().getPOSTag()));
 				
 				/* Dependency labels */
 				vector.addFeature(CurrentDEPLabel, getStringLabel(node1.getLabel(), node2.getLabel()));
 				
-				/* --- Positional features --- */
-				/* Token position in sentence */
-				vector.addFeature(TokenSentencePos, getStringLabel(node1.getID(), node2.getID()));
+				/* SubTree tokens, POS tags, and dependency labels */
+				for(DEPNode node : mention1.getSubTreeNodes()){
+					vector.addFeature(SubTreeTokens1, node.getWordForm().toLowerCase());
+					vector.addFeature(SubTreePOSTag1, node.getPOSTag());
+					vector.addFeature(SubTreeDEPlabel1, node.getLabel());
+				}
+				for(DEPNode node : mention2.getSubTreeNodes()){
+					vector.addFeature(SubTreeTokens2, node.getWordForm().toLowerCase());
+					vector.addFeature(SubTreePOSTag2, node.getPOSTag());
+					vector.addFeature(SubTreeDEPlabel2, node.getLabel());
+				}
+				
+				/* SubTree token overlap count*/
+				vector.addFeature(SubTreeTokenOverlap, Integer.toString(CoreferenceDSUtils.getOverlapCount(mention1.getSubTreeWordList(true), mention2.getSubTreeWordList(true), false)));
+				
+				/* Acronym */
+				if(CoreferenceStringUtils.getAllUpperCaseLetters(mention1.getSubTreeWordSequence()).equals(CoreferenceStringUtils.getAllUpperCaseLetters(mention1.getSubTreeWordSequence())))
+					vector.addFeature(Boolean, Integer.toString(AcronymMatch));
+				
+				/* Synonym */
+				if(wordnetDb.hasSameSynonym(wordForm1, wordForm2))
+					vector.addFeature(Boolean, Integer.toString(SynonymMatch));
+				
+				/* Antonym */
+				if(wordnetDb.isAntonyms(wordForm1, wordForm2))
+					vector.addFeature(Boolean, Integer.toString(AntonymMatch));
+				
+				/* Hypernym */
+				if(wordnetDb.hasSameHypernym(wordForm1, wordForm2))
+					vector.addFeature(Boolean, Integer.toString(HypernymMatch));
+				
+				/* Modifier sets */
+				for(String modifier : mention1.getModifiersList())	vector.addFeature(ModifierSet1, modifier);
+				for(String modifier : mention2.getModifiersList())	vector.addFeature(ModifierSet2, modifier);
+				
+				/* X-Pronoun pairs */
+				if(pronoun1 != null){
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getWordFrom(), node2.getPOSTag()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getWordFrom(), mention2.getEntityType().toString()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getWordFrom(), mention2.getGenderType().toString()));
+				}
+				if(pronoun2 != null){
+					vector.addFeature(XPronounPair, getStringLabel(node1.getPOSTag(), mention2.getWordFrom()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getEntityType().toString(), mention2.getWordFrom()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getGenderType().toString(), mention2.getWordFrom()));
+				}
 				
 				break;
 			
 			case 0:		// Only mention1 is multiple mention
 				AbstractMention tempMention = mention1;
 				mention1 = mention2; mention2 = tempMention;
-				DEPTree tempTree = tree1;
-				tree1 = tree2; tree2 = tempTree;
+				tree1 = mention1.getTree(); tree2 = mention2.getTree();
+				wordForm1 = mention1.getWordFrom(); wordForm2 = mention2.getWordFrom();
+				pronoun1 = mention1.getPronounType(); pronoun2 = mention2.getPronounType();
 				/* ... extraction continues in case 1 ... */
 			
 			case 1:		// Only mention2 is multiple mention
-				/* Pronoun type of mention1 */
-				pronoun1 = mention1.getPronounType();
-				vector.addFeature(PronounType, (pronoun1 == null)? NULL : pronoun1.toString());
+				node1 = mention1.getNode(); node2 = mention2.getNode();
+				
+				/* X-Pronoun pairs */
+				if(pronoun2 != null){
+					vector.addFeature(XPronounPair, getStringLabel(node1.getPOSTag(), mention2.getWordFrom()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getEntityType().toString(), mention2.getWordFrom()));
+					vector.addFeature(XPronounPair, getStringLabel(mention1.getGenderType().toString(), mention2.getWordFrom()));
+				}
 				
 				break;
 			
 			case 2:		// Both are multiple mentions
-				/* Match sizes of the multiple mentions */
-				vector.addFeature(MultiMentionSizes, getStringLabel(mention1.getSubMentions().size(), mention2.getSubMentions().size()));
+				/* Word from match */
+				if(wordForm1.equals(wordForm2))	vector.addFeature(Boolean, Integer.toString(MultiMentionWordForm));
 				
 				break;
 		}
-				
+		
 		return vector;
 	}
 	
-	private String getBinaryLabel(boolean bool){
-		return (bool)? TRUE : FALSE;
-	}
-	
 	private String getStringLabel(int... strings){
-//		s_builder.setLength(0);
-		s_builder = new StringBuilder();
+		s_builder.setLength(0);
 		for(int string : strings){
 			s_builder.append(string);
 			s_builder.append(CharConst.HYPHEN);
@@ -142,8 +175,7 @@ public class CoreferenceFeatureExtractor implements FeatureType{
 	}
 	
 	private String getStringLabel(String... strings){
-//		s_builder.setLength(0);
-		s_builder = new StringBuilder();
+		s_builder.setLength(0);
 		for(String string : strings){
 			s_builder.append(string);
 			s_builder.append(CharConst.HYPHEN);
